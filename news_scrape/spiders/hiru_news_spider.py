87c981bc42ae1502e0154be21d0a585215694f0b
Sidath Asiri <140398l@uom.lk>
2018-06-18 14:36:33 +0530

    updated hiru news spider for all news categories

--- news_scrape/spiders/hiru_news_spider.py
+++ news_scrape/spiders/hiru_news_spider.py
@@ -3,24 +3,30 @@ import scrapy
 class NewsSpider(scrapy.Spider):
     name = "hiru"
     start_urls = [
+        'http://www.hirunews.lk/all-news.php?pageID=1',
         'http://www.hirunews.lk/sports/all-sports-news.php?pageID=1',
+        'http://www.hirunews.lk/business/all-business-news.php?pageID=1',
+        'http://www.hirunews.lk/entertainment/all-entertainment-news.php?pageID=1'
     ]
 
     def parse(self, response):
         for i in range(len(response.css('div.middle-box div.rp-ltsbx div.rp-mian div.lts-cntp a::text').extract())):
             title = response.css('div.middle-box div.rp-ltsbx div.rp-mian div.lts-cntp a::text')[i].extract()
-            date = date = response.css('div.middle-box div.rp-ltsbx div.rp-mian div.time::text')[i].extract()
+            date = response.css('div.middle-box div.rp-ltsbx div.rp-mian div.time::text')[i].extract()
             date = date.split(',')[1].strip().split(' ')
             date = date[0]+" "+date[1]+", "+date[2]
+            category = response.url.split('/')[3]
+            if("news" in category):
+                category = "local"
             yield {
                 'text': title,
                 'date': date,
-                'source': 'Hiru News',
-                'category': 'Sports'
+                'category': category
+
             }
 
         current_page_number = int(response.css('div.pagi div.pagi_2 b::text').extract_first())
         print("currenttttttttttttttttttttttttttttttttttt", current_page_number)
         next_page = response.url[:-1]+str((current_page_number+1))
-        if(current_page_number<5):
+        if(current_page_number<8):
             yield scrapy.Request(next_page, callback=self.parse)
